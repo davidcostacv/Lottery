ccee9e395a37dd545d758644b510cbc172bd9246
davidcostacv <davidcostacv@gmail.com>
2022-08-07 22:50:49 -0400

    first commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'upset boost stereo develop foil brave sudden vault forest stamp lounge release',
+  // mnemonic
+  'https://rinkeby.infura.io/v3/b7513c34724948fa84f989bbdc732ffb'
+  // infura endpoint
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
